52041a57b662ba1919f31dcbeb1646fe9cb2eb7a
Jan <jan.kaminsky@mail.mcgill.ca>
2020-12-03 21:46:45 -0500

    copyright add

--- js/indexScript.js
+++ js/indexScript.js
@@ -5,3 +5,4 @@ tl.to('.slider', {y:'-100%', duration: 1.5, delay: 0.5});
 tl.to('.intro', {y:'-100%', duration: 1}, "-=1.1");
 tl.fromTo('.logo', {opacity:0}, {opacity: 1, duration: 1});
 tl.fromTo('.main-content', {opacity:0}, {opacity: 1, duration: 1}, '-=1');
+tl.fromTo('footer',  {opacity:0}, {opacity: 1, duration: 1}, '-=1');
--- js/tigerScript.js
+++ js/tigerScript.js
@@ -2,3 +2,4 @@ const tl = gsap.timeline({defaults: {ease: 'power1.out'} });
 
 tl.to('.slider', {y:'0%', duration: 1});
 tl.fromTo('.logo', {opacity:0}, {opacity: 1, duration: 2.5});
+tl.fromTo('footer', {opacity:0}, {opacity: 1, duration: 2.5}, '-=2.5');
