046ab19ed1bf34c4452f6e4cc34d5777437bf173
Jan <jan.kaminsky@mail.mcgill.ca>
2020-12-04 12:29:38 -0500

    "DOPE CHANGES" -Angus

--- js/tigerScript.js
+++ js/tigerScript.js
@@ -1,5 +1,5 @@
-const tl = gsap.timeline({defaults: {ease: 'power1.out'} });
+const tl = gsap.timeline({ defaults: { ease: 'power1.out' } });
 
-tl.to('.slider', {y:'0%', duration: 1});
-tl.fromTo('.logo', {opacity:0}, {opacity: 1, duration: 2.5});
-tl.fromTo('footer', {opacity:0}, {opacity: 1, duration: 2.5}, '-=2.5');
+tl.to('.slider', { y: '0%', duration: 1 });
+tl.fromTo('.logo', { opacity: 0 }, { opacity: 1, duration: 2.5 });
+tl.fromTo('footer', { opacity: 0 }, { opacity: 1, duration: 2.5 }, '-=2.5');
